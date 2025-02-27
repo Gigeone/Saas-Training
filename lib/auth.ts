f60042b5ed1966ba13fefd8f8fd125fd0be3ceff
Gigeone <matthieugh@gmail.com>
2025-02-27 21:21:10 +0100

    bug correction

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,12 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
+const prisma = new PrismaClient();
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
 });
